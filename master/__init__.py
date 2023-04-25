943cdb5d6c1b1e4654e4d7fc2589c3381413f343
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2023-04-25 06:39:23 +0200

    stable -> update from version 0.1.0 to 0.2.0

--- master/__init__.py
+++ master/__init__.py
@@ -1,5 +1,5 @@
 # The viable is usually _version_ and you should use semantic versioning, which means first number is the major
 # number, stable API, second number is a minor version, editions only, and the last one is patch level.
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 # Keep _init_.py clear of code and import all the functions and classes from other sub-models
